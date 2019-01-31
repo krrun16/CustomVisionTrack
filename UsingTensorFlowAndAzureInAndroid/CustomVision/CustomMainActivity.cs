6807c957bbdba88b65bc9d39391f26e108ceb678
Rumi, Masuma Akter <masumaakter-rumi@uiowa.edu>
2019-01-31 16:31:01 -0600

    uploading our current mobile app to github

---
+++ UsingTensorFlowAndAzureInAndroid/CustomVision/CustomMainActivity.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.Hardware;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace CustomVision
+{
+    [Activity(Label = "@string/app_name", MainLauncher = true, Icon = "@mipmap/icon", Theme = "@style/MyTheme")]
+
+    public class CustomMainActivity : AppCompatActivity, Android.Hardware.Camera.IPictureCallback, 
+        Android.Hardware.Camera.IPreviewCallback, Android.Hardware.Camera.IShutterCallback, ISurfaceHolderCallback
+    {
+        public Android.Support.V7.Widget.Toolbar Toolbar { get; set; }
+
+        public void OnPictureTaken(byte[] data, Android.Hardware.Camera camera)
+        {
+            throw new NotImplementedException();
+        }
+        
+
+        public void OnPreviewFrame(byte[] data, Android.Hardware.Camera camera)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnShutter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SurfaceChanged(ISurfaceHolder holder, [GeneratedEnum] Format format, int width, int height)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SurfaceCreated(ISurfaceHolder holder)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SurfaceDestroyed(ISurfaceHolder holder)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.custom_main);
+
+            Toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            if (Toolbar != null)
+                SetSupportActionBar(Toolbar);
+
+            SupportActionBar.SetDisplayHomeAsUpEnabled(false);
+            SupportActionBar.SetHomeButtonEnabled(false);
+            SurfaceView surface = (SurfaceView) FindViewById(Resource.Id.surface);
+
+        }
+    }
+}
---
+++ UsingTensorFlowAndAzureInAndroid/CustomVision/obj/Debug/81/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("CustomVision.MainApplication, CustomVision, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md5784075f5b76665e2a0849cb69ccc4ca2.MainApplication.class, md5784075f5b76665e2a0849cb69ccc4ca2.MainApplication.__md_methods);
+		
+	}
+}
